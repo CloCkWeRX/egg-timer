6b0d5f861583d1fa1fb5b5c069dd7227e82d05f1
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-02-08 19:47:55 +1030

    Import

---
+++ reminder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+counter=0
+echo -n "How many minutes would you like the timer to run? "
+read limit
+echo -n "Summary?"
+read summary
+echo -n "Start message?"
+read startmessage
+echo -n "Done message?"
+read endmessage
+notify-send -u critical -i appointment -t 600 "$summary" "$startmessage"
+echo
+while [ $counter != $limit ]; do
+   echo "$counter minutes so far...";
+   sleep 60
+   let "counter = $counter + 1"
+done
+if [ $counter = $limit ]; then
+   echo
+   notify-send -u critical -i appointment "$summary" "$endmessage"
+   echo -e '\a' &gt;&amp;2
+   exit 0
+fi
